62f1e1d18b80b684e2577e1be47b8656947c36d0
ghp_FcHg1K2C8HRmGh2E86WLr5TVPpXTYT4UTIo9 <dennisphori2@gmail.com>
2022-09-08 16:08:11 -0700

    print out using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,13 @@
 #include <stdio.h>
 
-
-
-/*
- *
+/**
  * main - Entry point
  *
- *Description: using main function
- *
- * Return 0
- *
+ *Description: Using main function
+ * Return: 0
  */
-
-
-
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
