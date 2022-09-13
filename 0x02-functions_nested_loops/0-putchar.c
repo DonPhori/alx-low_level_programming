cd6c5cc6fe81fcc3396b9f3b983b4704d8eaf0cc
ghp_FcHg1K2C8HRmGh2E86WLr5TVPpXTYT4UTIo9 <dennisphori2@gmail.com>
2022-09-13 15:52:47 -0700

    print a pointer

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	const char *print = "putchar";
+
+	printf("_%s", print);
+	return (0);
+}
