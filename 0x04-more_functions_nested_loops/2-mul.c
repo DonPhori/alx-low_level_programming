4754ec810dc43f133f6f588a0218a92b421cadf4
ghp_FcHg1K2C8HRmGh2E86WLr5TVPpXTYT4UTIo9 <dennisphori2@gmail.com>
2022-09-15 16:31:08 -0700

    function that produces the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - returns the product of two integers
+ * @a: an integer
+ * @b: and integer
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
