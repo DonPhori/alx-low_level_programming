383709eeeef3e36f72c39914f741484c2f771c51
ghp_FcHg1K2C8HRmGh2E86WLr5TVPpXTYT4UTIo9 <dennisphori2@gmail.com>
2022-10-05 06:32:41 -0700

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - entry point
+ * @str: string to duplicate
+ * Return: a pointer to the duplicated string or null
+ */
+
+char *_strdup(char *str)
+{
+	char *strnew = NULL;
+	unsigned int i;
+	int n;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0; str[n] != '\0'; n++)
+	{
+		;
+	}
+	strnew = (char *)malloc(n + 1 * sizeof(char));
+	if (strnew != NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			strnew[i] = str[i];
+		}
+	} else
+	{
+		return (NULL);
+	}
+	strnew[i] = '\0';
+	return (strnew);
+}
