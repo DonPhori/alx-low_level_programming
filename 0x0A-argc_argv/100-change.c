6f081ddeaf40d04bec9684b190e44af9980817b6
ghp_FcHg1K2C8HRmGh2E86WLr5TVPpXTYT4UTIo9 <dennisphori2@gmail.com>
2022-10-05 00:43:00 -0700

    print minimum amoutn of coins needed to make change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int change(int cents);
+/**
+ * change - function to get change
+ * @cents: amount of coins
+ * Return: change
+ */
+
+int change(int cents)
+{
+	int q = 25;
+	int d = 10;
+	int n = 5;
+	int t = 2;
+	int p = 1;
+	int coins;
+
+	while (cents > 0)
+	{
+		while (cents >= q)
+		{
+			cents -= q;
+			coins++;
+		}
+		while (cents >= d)
+		{
+			cents -= d;
+			coins++;
+		}
+		while (cents >= n)
+		{
+			cents -= n;
+			coins++
+		}
+		while (cents >= t)
+		{
+			cents -= t;
+			coins++;
+		}
+		while (cents >= p)
+		{
+			cents -= p;
+			coins++;
+		}
+	}
+	return (coins);
+}
 
 /**
  * main - Entry point
