8332b72d224ed12f8a4e0d2c90805f61e362d191
ghp_FcHg1K2C8HRmGh2E86WLr5TVPpXTYT4UTIo9 <dennisphori2@gmail.com>
2022-10-05 03:30:11 -0700

    minimum number of coins needed to make change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,72 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int change(int cents);
-
 /**
  * main - Entry point
  * @argc: Argument count
  * @argv: argument vector
- * Return: Always 0
+ * Return: Always 0 on success and 1 on error
  */
 
 int main(int argc, char **argv)
 {
+	int cents;
+	int coins;
+	int remainder;
+
+	int quarters, dimes, nickels, twos, pennies;
+
 	if (argc != 2)
 	{
-		printf("%s\n", "Error");
+		printf("Error\n");
 		return (1);
 	}
-	else if (argc < 0)
+	else
 	{
+		cents = atoi(argv[1]);
+	}
+	if (cents <= 0)
+	{
+		printf("0\n");
 		return (0);
 	}
-	printf("%d\n", change(atoi(argv[1])));
+	quarters = cents / 25;
+	remainder = cents % 25;
+	dimes = remainder / 10;
+	remainder = remainder % 10;
+	nickels = remainder / 5;
+	remainder = remainder % 5;
+	twos = remainder / 2;
+	remainder = remainder % 2;
+	pennies = remainder / 1;
+
+	coins = quarters + dime + nickels + twos + pennies;
+
+	printf("%d\n", coins);
 	return (0);
-}
-
-/**
- * change - amount of change
- * @cents: amount of coins rturned from main
- * Return: change
- */
-
-int change(int cents)
-{
-	int q = 25;
-	int d = 10;
-	int n = 5;
-	int t = 2;
-	int p = 1;
-	int coins;
-
-	while (cents > 0)
-	{
-		while (cents >= q)
-		{
-			cents -= q;
-			coins++;
-		}
-		while (cents >= d)
-		{
-			cents -= d;
-			coins++
-		}
-		while (cents >= n)
-		{
-			cents -= n;
-			coins++;
-		}
-		while (cents >= t)
-		{
-			cents -= t;
-			coins++;
-		}
-		while (cents >= p)
-		{
-			cents -= p;
-			coins++;
-		}
-	}
-	return (coins);
 }
