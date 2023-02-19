1674b2f3ab5888d18248ed38ce307c7036e94da6
DonPhori <dennisphori2@gmail.com>
2023-02-19 04:39:20 -0800

    using the write function

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/* include header files for preprocessor */
+#include <stdio.h>
+
+/**
+ * main - start of program
+ *
+ * write() - function to write data out of a buffer
+ *
+ * Return: 1
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 89);
+	return (1);
+}
