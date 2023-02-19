b887bb49c0b20d7709e7a404f8b1d392761bcb70
DonPhori <dennisphori2@gmail.com>
2023-02-19 01:33:45 -0800

    using the printf() function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+/* import header files */
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * printf() - function to print string to stdout
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
