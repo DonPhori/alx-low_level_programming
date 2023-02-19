4eb126383938d69c6c60441ddb452ac4e4adb136
DonPhori <dennisphori2@gmail.com>
2023-02-19 01:28:10 -0800

    print text using the puts() function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/* Import header file input output */
+#include <stdio.h>
+
+/**
+ * main - Prints a string
+ *
+ * puts() - funcion used to write a string to stdout
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
