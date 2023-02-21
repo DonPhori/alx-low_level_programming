88e4b3365eb725f3a4fffb537696bd01dee797ec
DonPhori <dennisphori2@gmail.com>
2023-02-21 07:16:55 -0800

    print numbers from 0-9 using putchar and the ASCII table indexing

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+/* include header libraries */
+#include <stdio.h>
+
+/**
+ * main - function to print numbers from 0 - 9 followed be a newline
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
