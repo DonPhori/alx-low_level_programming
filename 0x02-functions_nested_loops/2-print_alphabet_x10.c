f34dfaf6296bb67832ef903f85fabb8c3474d805
DonPhori <dennisphori2@gmail.com>
2023-02-22 02:45:23 -0800

    correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,7 +10,7 @@ void print_alphabet_x10(void)
 	int count = 0;
 	char alpha = 'a';
 
-	while (count <= 9)
+	while (count++ <= 9)
 	{
 		while (alpha <= 'z')
 		{
@@ -18,6 +18,5 @@ void print_alphabet_x10(void)
 			alpha++;
 		}
 		_putchar('\n');
-		count++;
 	}
 }
