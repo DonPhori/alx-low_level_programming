0946a7adfcb19c2ad0fc9bbfda81703b1a5f4346
DonPhori <dennisphori2@gmail.com>
2023-02-22 02:43:02 -0800

    define the print_alphabet function before calling it

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,16 +3,21 @@
 
 /**
  * print_alphabet_x10 - function to print alphabet 10 times
- * print_alphabet - function to print alphabet
  */
 
 void print_alphabet_x10(void)
 {
 	int count = 0;
+	char alpha = 'a';
 
 	while (count <= 9)
 	{
-		print_alphabet();
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('\n');
 		count++;
 	}
 }
