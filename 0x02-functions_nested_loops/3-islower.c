3baa82940ce8e188d8216d24f07f6a1d9f0ff428
DonPhori <dennisphori2@gmail.com>
2023-02-22 05:51:32 -0800

    declare different variable as the type

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,13 +3,15 @@
 
 /**
  * _islower - function to to check for lowercase
+ * @c: character which will be compared
+ * Return: 0 if false and 1 if true
  */
 
 int _islower(int c)
 {
-	int c;
+	int x;
 
-	if (c >= 97 && c <= 122)
+	if (x >= 97 && x <= 122)
 	{
 		return (1);
 	}
