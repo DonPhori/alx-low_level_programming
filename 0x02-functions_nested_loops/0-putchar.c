6a37b2acb5d038483938f61aa243478dbffa8863
DonPhori <dennisphori2@gmail.com>
2023-02-21 23:08:53 -0800

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+/* include header libraries */
+#include <stdio.h>
+
+/**
+ * main - function to print _putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
