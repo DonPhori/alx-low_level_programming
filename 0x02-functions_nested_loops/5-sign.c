7ff3ab3b445da7958e6b91ffbde1907ed72ed88c
DonPhori <dennisphori2@gmail.com>
2023-02-22 06:31:39 -0800

    function to determine if positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+/* include header library containing prototypes */
+#include "main.h"
+
+/**
+ * print_sign - function to check if a number is positive or negative
+ * @n: Number to be compared
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else
+	{
+		printf("0");
+		return (0);
+	}
+}
