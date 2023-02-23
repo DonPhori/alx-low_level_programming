5178e150d7056bf24162e8391f6dd53d5d9779f6
DonPhori <dennisphori2@gmail.com>
2023-02-23 03:25:21 -0800

    add spaces

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - function to print natural numbers
@@ -11,7 +12,7 @@ void print_to_98(int n)
 	{
 		while (n > 98)
 		{
-			printf("%d", n--);
+			printf("%d, ", n--);
 		}
 		printf("%d\n", n);
 	}
@@ -19,7 +20,7 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			printf("%d", n++);
+			printf("%d, ", n++);
 		}
 		printf("%d\n", n);
 	}
