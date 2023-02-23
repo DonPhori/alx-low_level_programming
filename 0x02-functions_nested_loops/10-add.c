f76607588ad41e164b7c5a043087d007c9756a1f
DonPhori <dennisphori2@gmail.com>
2023-02-23 03:04:58 -0800

    definition of the add function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/*include heade library */
+#include "main.h"
+
+/**
+ * add - function to add integers
+ * @a: number to be added
+ * @b: number to be added
+ * Return: The results
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
