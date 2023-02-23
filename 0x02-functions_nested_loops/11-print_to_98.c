ff9761a03bb950e844d31c663ada76674727ce3b
DonPhori <dennisphori2@gmail.com>
2023-02-23 03:20:01 -0800

    print whole numbers until 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_to_98 - function to print natural numbers
+ * @n: number passed to function
+ */
+
+void print_to_98(int n)
+{
+	if (n != 98)
+	{
+		while (n > 98)
+		{
+			printf("%d", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d", n++);
+		}
+		printf("%d\n", n);
+	}
+}
