8cc86f6a85a598c0fab42e4e3ffb64b1a4ea52dd
DonPhori <dennisphori2@gmail.com>
2023-02-23 02:55:40 -0800

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,16 +9,27 @@ void times_table(void)
 {
 	int m;
 	int n;
-	int a = n * m;
+	int a;
 
 	for (n = 0; n <= 9; n++)
 	{
-		for (m = 0; m <= 9; m++)
+		_putchar('0');
+		for (m = 1; m <= 9; m++)
 		{
-			_putchar(a);
 			_putchar(',');
 			_putchar(' ');
+
+			a = n * m;
+			if (a <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((a / 10) + '0');
+			}
+			_putchar((a % 10) + '0');
 		}
 		_putchar('\n');
 	}
-}	
+}
