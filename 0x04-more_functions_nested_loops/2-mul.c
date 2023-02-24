28ee585c06ea7d64824bccab00aa1db9d3baadb8
DonPhori <dennisphori2@gmail.com>
2023-02-23 23:12:03 -0800

    mul function definition

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply 2 int values
+ * @a: value 1
+ * @b: value 2
+ * Return: The product of a & b
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
