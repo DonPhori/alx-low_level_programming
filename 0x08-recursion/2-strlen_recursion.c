7f226c4f2f4ef94d64d7c05bdb35fb9848729d9f
DonPhori <dennisphori2@gmail.com>
2023-03-11 01:11:56 -0800

    strlen function definition using recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function to print string length
+ * @s: String
+ * Return: 0 on success
+ * and 1 if need to continue
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	_strlen_recursion(s + 1);
+}
