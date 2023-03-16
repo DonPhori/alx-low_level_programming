91450974fc09c000e76df435eb3243f7f442bd2f
DonPhori <dennisphori2@gmail.com>
2023-03-16 08:26:53 -0700

    program to count number of arguments after the program name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point to program
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int args, i;
+
+	for (i = 0; i < argc; i++)
+	{
+		args = i;
+	}
+	printf("%d\n", args);
+
+	return (0);
+}
