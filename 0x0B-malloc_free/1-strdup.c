3bc53f2401c883515a67e0c705694b552011ac51
DonPhori <dennisphori2@gmail.com>
2023-03-21 06:29:20 -0700

    _strdup() function definition

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function to duplicate a string
+ * @str: string to be duplicated
+ * Return: NULL if str = null
+ * return pointer to to str
+ */
+
+char *_strdup(char *str)
+{
+	/* initialize string length to 0 */
+	unsigned int length = 0;
+	unsigned int i;
+
+	if (str == '\0')
+	{
+		return (NULL);
+	}
+	/* get the length of str */
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	/* allocate memory for the new str */
+	char *new_str = malloc(length + 1);
+	/* make sure memory was allocated */
+	if (new_str != NULL)
+	{
+		for (i = 0; i < length; i++)
+		{
+			new_str[i] = str[i];
+		}
+	}
+	return (new_str);
+}
